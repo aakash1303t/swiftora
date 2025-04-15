3edb3724e9ce36d5a19c4923bb021a8705cd6460
aakash1303t <aakash93610t@gmail.com>
2025-04-15 12:13:09 +0530

    frontend-update-folder-02

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import logo from "../assets/logo1.png";
+
 const Registration = () => {
   const [formData, setFormData] = useState({
     username: "",
